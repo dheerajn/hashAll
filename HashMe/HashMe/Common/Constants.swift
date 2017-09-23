ea3a6c9a945e6c794e47673d142df7d3d9fbcf4b
dheeru <dheerajneelam@gmail.com>
2017-09-23 13:05:58 -0400

    #comment added constants file

---
+++ HashMe/HashMe/Common/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  HashMe
+//
+//  Created by Dheeru on 9/23/17.
+//  Copyright © 2017 Dheeru. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static var onboaringIdentifier = "OnBoarding"
+}
