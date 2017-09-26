ee8a70f85464ff68bdaf57699b396813702bb488
dheeru <dheerajneelam@gmail.com>
2017-09-26 19:42:38 -0400

    #comment added the base lines for the animation

--- HashMe/HashMe/Common/UI/CustomLoadingAnimation.swift
+++ HashMe/HashMe/Common/UI/CustomLoadingAnimation.swift
@@ -9,6 +9,12 @@
 import Foundation
 import UIKit
 
+//    |   |
+//  --| - |--
+//    |   |
+//  --| - |--
+//    |   |
+
 open class CustomLoadingAnimation: UIView {
     
     fileprivate weak var shapeLayer1: CAShapeLayer?
@@ -19,25 +25,30 @@ open class CustomLoadingAnimation: UIView {
     fileprivate let squareView = UIView()
     fileprivate let animationDuration = 0.25
     
+    /// This helps you not drawing the base lines for the animation more than once.
+    fileprivate var initialLoad: Bool = true
+    
     open func startLoadingAnimation() {
         squareView.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height)
         squareView.alpha = 1
         squareView.backgroundColor = UIColor.LightGreyBackgroundColor()
         self.addSubview(squareView)
         
-        startLoadingAnimationMain(fromX: self.center.x - 25, fromY: self.center.y - 50, toX: self.center.x - 25, toY: self.center.y + 50)
+        drawBaseLinesForAnimation()
+        
+        self.startLoadingAnimation(fromX: self.center.x - 25, fromY: self.center.y - 50, toX: self.center.x - 25, toY: self.center.y + 50)
         
         let pause = animationDuration
         DispatchQueue.main.asyncAfter(deadline: TimeInterval.convertToDispatchTimeT(pause), execute: {
-            self.startLoadingAnimationMain(fromX: self.center.x + 25, fromY: self.center.y - 50, toX: self.center.x + 25, toY: self.center.y + 50)
+            self.startLoadingAnimation(fromX: self.center.x + 25, fromY: self.center.y - 50, toX: self.center.x + 25, toY: self.center.y + 50)
         })
         
         DispatchQueue.main.asyncAfter(deadline: TimeInterval.convertToDispatchTimeT(pause * 2), execute: {
-            self.startLoadingAnimationMain(fromX: self.center.x + 55, fromY: self.center.y - 20, toX: self.center.x - 55, toY: self.center.y - 20)
+            self.startLoadingAnimation(fromX: self.center.x + 55, fromY: self.center.y - 20, toX: self.center.x - 55, toY: self.center.y - 20)
         })
         
         DispatchQueue.main.asyncAfter(deadline: TimeInterval.convertToDispatchTimeT(pause * 3), execute: {
-            self.startLoadingAnimationMain(fromX: self.center.x + 55, fromY: self.center.y + 20, toX: self.center.x - 55, toY: self.center.y + 20)
+            self.startLoadingAnimation(fromX: self.center.x + 55, fromY: self.center.y + 20, toX: self.center.x - 55, toY: self.center.y + 20)
         })
         
         DispatchQueue.main.asyncAfter(deadline: TimeInterval.convertToDispatchTimeT(pause * 4), execute: {
@@ -52,7 +63,7 @@ open class CustomLoadingAnimation: UIView {
         })
     }
     
-    fileprivate func startLoadingAnimationMain(fromX: CGFloat, fromY: CGFloat, toX: CGFloat, toY: CGFloat) {
+    fileprivate func startLoadingAnimation(fromX: CGFloat, fromY: CGFloat, toX: CGFloat, toY: CGFloat, animation: Bool = true, strokeColor: UIColor = UIColor.red) {
         
         // remove old shape layer if any
         //        self.shapeLayer?.removeFromSuperlayer()
@@ -65,18 +76,32 @@ open class CustomLoadingAnimation: UIView {
         // create shape layer for that path
         let shapeLayer = CAShapeLayer()
         shapeLayer.fillColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0).cgColor
-        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.strokeColor = strokeColor.cgColor
         shapeLayer.lineWidth = 4
         shapeLayer.path = path.cgPath
         
         // animate it
         squareView.layer.addSublayer(shapeLayer)
-        let animation = CABasicAnimation(keyPath: "strokeEnd")
-        animation.fromValue = 0
-        animation.duration = animationDuration
-        shapeLayer.add(animation, forKey: "MyAnimation")
         
-        assignShapeLayer(shapeLayer: shapeLayer)
+        if animation {
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.fromValue = 0
+            animation.duration = animationDuration
+            shapeLayer.add(animation, forKey: "MyAnimation")
+            
+            assignShapeLayer(shapeLayer: shapeLayer)
+        }
+    }
+    
+    /// This Method helps in drawing (only once) the base line for the animation.
+    fileprivate func drawBaseLinesForAnimation() {
+        if initialLoad {
+            initialLoad = false
+            self.startLoadingAnimation(fromX: self.center.x - 25, fromY: self.center.y - 50, toX: self.center.x - 25, toY: self.center.y + 50, animation: false, strokeColor: UIColor.lightGray)
+            self.startLoadingAnimation(fromX: self.center.x + 25, fromY: self.center.y - 50, toX: self.center.x + 25, toY: self.center.y + 50, animation: false, strokeColor: UIColor.lightGray)
+            self.startLoadingAnimation(fromX: self.center.x + 55, fromY: self.center.y - 20, toX: self.center.x - 55, toY: self.center.y - 20, animation: false, strokeColor: UIColor.lightGray)
+            self.startLoadingAnimation(fromX: self.center.x + 55, fromY: self.center.y + 20, toX: self.center.x - 55, toY: self.center.y + 20, animation: false, strokeColor: UIColor.lightGray)
+        }
     }
     
     fileprivate func assignShapeLayer(shapeLayer: CAShapeLayer) {
