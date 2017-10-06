b11907e70e144a356574b115acfe4da75d19fa48
dheeru <dheerajneelam@gmail.com>
2017-10-05 23:05:27 -0400

    #comment added Facebook sdk

--- HashMe/HashMe/Extras/AppDelegate.swift
+++ HashMe/HashMe/Extras/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,12 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let mainController = RootViewController()
         let navigationController = UINavigationController(rootViewController: mainController)
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(app,
+                                                                     open: url,
+                                                                     sourceApplication: UIApplicationOpenURLOptionsKey.sourceApplication.rawValue,
+                                                                     annotation: UIApplicationOpenURLOptionsKey.annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -38,6 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
