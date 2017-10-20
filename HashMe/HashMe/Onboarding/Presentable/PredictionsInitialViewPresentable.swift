f7f554739c260e6a42dd06acf3b6e4d9300b2ca2
dheeru <dheerajneelam@gmail.com>
2017-10-19 21:49:52 -0400

    #comment added a new group for presentable and renamed file

---
+++ HashMe/HashMe/Onboarding/Presentable/PredictionsInitialViewPresentable.swift
@@ -0,0 +1,25 @@
+//
+//  PredictionsInitialViewPresentable.swift
+//  HashMe
+//
+//  Created by Dheeru on 10/19/17.
+//  Copyright © 2017 Dheeru. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+public protocol PredictionsInitialViewPresentable: class {
+    func showPredictionsView()
+}
+
+public extension PredictionsInitialViewPresentable where Self: HashTagFlowController {
+    func showPredictionsView() {
+        let storyboard = UIStoryboard(name: Constants.mainStoryboardIdentifier, bundle: nil)
+        let hashTagVc = storyboard.instantiateViewController(withIdentifier: Constants.hashtagsVcIdentifier) as? PredictionsViewController
+        hashTagVc?.flowDelegate = self
+        hashTagVc?.viewModel = PredictionsViewModel(flowDelegate: self)
+        self.pushViewControllerWithAnimation(hashTagVc!, withAnimationType: .fade)
+    }
+}
+
