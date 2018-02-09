74f12bdc80d4d462bc090b0f794adda74c1edb11
Dheeru <dheerajneelam@gmail.com>
2018-02-09 18:53:48 -0500

    #comment added a comment

--- HashMe/HashMe/Common/Protocols/DeviceIdiom.swift
+++ HashMe/HashMe/Common/Protocols/DeviceIdiom.swift
@@ -14,6 +14,7 @@ public protocol DeviceIdiom {
     var isDeviceIphone: Bool { get }
 }
 
+// MARK: - DeviceIdiom Extension for HashTagFlowController
 public extension DeviceIdiom where Self: HashTagFlowController {
     
     var isDeviceIpad: Bool {
